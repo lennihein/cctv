be4bc8cd1a3cc8cb7fbb88a704cc3da17891734b
Lenni Hein <git@lennihein.com>
2023-06-22 19:52:04 +0200

    fixed start script

--- nix_run.sh
+++ nix_run.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash --pure
-#! nix-shell -p python3 util-linux systemd
-#! nix-shell -I https://github.com/NixOS/nixpkgs/archive/4ecab3273592f27479a583fb6d975d4aba3486fe.tar.gz
+#! nix-shell -p python310 util-linux systemd
+#! nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/4ecab3273592f27479a583fb6d975d4aba3486fe.tar.gz
 
 python3 main.py
